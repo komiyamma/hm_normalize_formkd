3d8698d9bd4b790bbd83c995051c08457d86436f
komiyamma <komiyama0629@gmail.com>
2023-05-18 14:04:37 +0900

    完全互換分解のみ効果を持ちそう

--- HmNormalizeFormKD/Program.cs
+++ HmNormalizeFormKD/Program.cs
@@ -6,15 +6,9 @@ using System.Text;
 [Guid("A5DCE2E1-B5F1-4FB0-AF2B-38FBC22E80E8")]
 public class HmNormalizeFormKD
 {
-    public string NormalizeKD(string str)
+    public string Normalize(string str)
     {
         return str.Normalize(NormalizationForm.FormKD);
     }
-
-    public string NormalizeD(string str)
-    {
-        return str.Normalize(NormalizationForm.FormD);
-    }
-
 }
 
