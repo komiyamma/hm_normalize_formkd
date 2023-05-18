da86c0585d988f2dc98473cfdb1f6bf07ccc4cfe
komiyamma <komiyama0629@gmail.com>
2023-05-18 14:03:49 +0900

    完全互換分解

---
+++ HmNormalizeFormKD/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+
+
+[Guid("A5DCE2E1-B5F1-4FB0-AF2B-38FBC22E80E8")]
+public class HmNormalizeFormKD
+{
+    public string NormalizeKD(string str)
+    {
+        return str.Normalize(NormalizationForm.FormKD);
+    }
+
+    public string NormalizeD(string str)
+    {
+        return str.Normalize(NormalizationForm.FormD);
+    }
+
+}
+
