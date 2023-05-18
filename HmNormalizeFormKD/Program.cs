f4f4a69f6e561c6c1ef8fa6a5732ed644d9c6df0
komiyamma <komiyama0629@gmail.com>
2023-05-18 15:04:56 +0900

    フォントになければ完全互換分解する

--- HmNormalizeFormKD/Program.cs
+++ HmNormalizeFormKD/Program.cs
@@ -1,14 +1,50 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Diagnostics;
+using System;
+using System.Runtime.InteropServices;
 using System.Text;
-
-
+using System.Windows.Media;
 
 [Guid("A5DCE2E1-B5F1-4FB0-AF2B-38FBC22E80E8")]
 public class HmNormalizeFormKD
 {
-    public string Normalize(string str)
+    public string Normalize(string str, string fontname)
     {
-        return str.Normalize(NormalizationForm.FormKD);
+        try
+        {
+            var fontFamily = new System.Windows.Media.FontFamily(fontname);
+            foreach (var typeface in fontFamily.GetTypefaces())
+            {
+                string sum = "";
+                GlyphTypeface gTypeface;
+                if (!typeface.TryGetGlyphTypeface(out gTypeface))
+                {
+                    return "Error";
+                }
+
+                var chars = str.ToCharArray();
+                foreach (var chr in chars)
+                {
+                    if (gTypeface.CharacterToGlyphMap.ContainsKey(chr))
+                    {
+                        sum += chr;
+                    }
+
+                    else
+                    {
+                        string temp = chr + "";
+                        sum += temp.Normalize(NormalizationForm.FormKD);
+                    }
+                }
+
+                return sum;
+            }
+
+            return "Error";
+        } catch(Exception ex)
+        {
+            Trace.WriteLine(ex);
+        }
+        return "Error";
     }
 }
 
